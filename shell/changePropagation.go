eac6b12df19e7ef40419ce43b94910cc5c61a7db
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-11-21 11:06:35 +0100

    chore: using path

--- shell/changePropagation.go
+++ shell/changePropagation.go
@@ -8,5 +8,6 @@ import (
 
 func PropagateChangesToCurrentShell(path string, logger *zap.SugaredLogger) error {
 	logger.Debugf("Propagating changes to current shell")
+	logger.Infof("Using path: %s", path)
 	return os.Setenv("PATH", path+string(os.PathListSeparator)+os.Getenv("PATH"))
 }
